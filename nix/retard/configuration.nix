afa784de87399511e630f5891fd1f829ecdf0718
Lucas Rasmussen <lucasnethaj@gmail.com>
2025-05-27 20:39:27 +0200

    update pulseaudio service config

--- nix/retard/configuration.nix
+++ nix/retard/configuration.nix
@@ -89,7 +89,7 @@
   xdg.portal.enable = true;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     # enable = true;
